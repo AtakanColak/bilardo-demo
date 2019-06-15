a2dad382c3540a0500b02140b0e48941d2b228ba
Ata <atakancolak9897@gmail.com>
2019-06-15 14:45:14 +0300

    Initialization
    Migrating the project to repository

---
+++ Assets/MusicScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Slider))]
+public class MusicScript : MonoBehaviour
+{
+
+    public AudioSource audioData;
+    public Slider slider;
+
+    public void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.volume = slider.value;
+        audioData.Play(0);
+        audioData.loop = true;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void SoundSlide()
+    {
+        audioData.volume = slider.value;
+    }
+}
