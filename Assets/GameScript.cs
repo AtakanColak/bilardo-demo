ccfe67049b46eb95c37004f004387b69141e8b15
Ata <atakancolak9897@gmail.com>
2019-06-16 14:39:35 +0300

    Camera Rotate

--- Assets/GameScript.cs
+++ Assets/GameScript.cs
@@ -7,29 +7,49 @@ using TMPro;
 
 public class GameScript : MonoBehaviour
 {
-
+    private Transform MainCamera;
+    private Transform whiteball;
     private TextMeshProUGUI timer;
     private float time;
+    private float sensitivity = 1;
+    private Vector3 dragOrigin;
+
+    private Vector3 cameraFocus()
+    {
+        return whiteball.position + new Vector3(0, 0.5f, 0);
+    }
 
     private void MainMenu()
     {
         SceneManager.LoadScene("Menu");
     }
 
-    // Start is called before the first frame update
     void Start()
     {
+        time = 0.0f;
         Button returnToMainMenu = GameObject.Find("ExitButton").GetComponent<Button>();
         returnToMainMenu.onClick.AddListener(delegate { MainMenu(); });
         timer = GameObject.Find("TimerText").GetComponent<TextMeshProUGUI>();
-        time = 0.0f;
-        Debug.Log("Started");
+        MainCamera = GameObject.Find("Main Camera").GetComponent<Camera>().transform;
+        whiteball = GameObject.Find("WhiteBall").transform;
+        MainCamera.position = whiteball.position + new Vector3(-2, 1f, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
         timer.text = time.ToString("0.0") + " s";
+        MainCamera.LookAt(cameraFocus());
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    dragOrigin = Input.mousePosition;
+        //    return;
+        //}
+        if (!Input.GetMouseButton(0)) { dragOrigin = Input.mousePosition;  return; }
+        Vector3 pos = Camera.main.ScreenToViewportPoint(dragOrigin - Input.mousePosition);
+
+        MainCamera.RotateAround(cameraFocus(), -Vector3.up, pos.x* sensitivity);
+        //float rotateHorizontal = Input.GetAxis("Mouse X");
+        //MainCamera.RotateAround(cameraFocus(), -Vector3.up, rotateHorizontal * sensitivity);
     }
 }
