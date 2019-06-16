379d0ec2864fb55c7c3c2006880720ba712eed79
Ata <atakancolak9897@gmail.com>
2019-06-16 11:52:59 +0300

    Find & OnSceneLoaded
    Started GameObject.Find and OnSceneLoaded

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,8 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
-
     public void PlayGame()
     {
         SceneManager.LoadScene("Game");
@@ -17,5 +15,4 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
-
 }
--- Assets/MusicScript.cs
+++ Assets/MusicScript.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 [RequireComponent(typeof(AudioSource))]
-[RequireComponent(typeof(Slider))]
 public class MusicScript : MonoBehaviour
 {
 
     public AudioSource audioData;
-    public Slider slider;
+    private Slider slider;
+
+    private void GetSlider() {
+        slider = GameObject.Find("SoundSlider").GetComponent<Slider>();
+    }
 
     public void Start()
     {
+        GetSlider();
+        SceneManager.sceneLoaded += OnNewSceneLoaded;
         audioData = GetComponent<AudioSource>();
         audioData.volume = slider.value;
         audioData.Play(0);
@@ -25,4 +31,15 @@ public class MusicScript : MonoBehaviour
     {
         audioData.volume = slider.value;
     }
+
+    void OnNewSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("OnSceneLoaded: " + scene.name);
+        Debug.Log(mode);
+    }
+
+    //public void ToogleMusic()
+    //{
+    //    audioData.mute = !audioData.mute;
+    //}
 }
