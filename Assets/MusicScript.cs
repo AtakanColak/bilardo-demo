ea4bb57fdf78250a26d60cff78aa265edfef9985
Ata <atakancolak9897@gmail.com>
2019-06-16 12:27:32 +0300

    Music Button Works

--- Assets/MusicScript.cs
+++ Assets/MusicScript.cs
@@ -11,14 +11,29 @@ public class MusicScript : MonoBehaviour
 
     public AudioSource audioData;
     private Slider slider;
+    private Image imgmusic;
+    private Sprite musicon;
+    private Sprite musicoff;
 
-    private void GetSlider() {
+    private void GetSlider()
+    {
         slider = GameObject.Find("SoundSlider").GetComponent<Slider>();
+        slider.onValueChanged.AddListener(delegate { SoundSlide(); });
+    }
+
+    private void GetToggler()
+    {
+        Button toggler = GameObject.Find("MusicButton").GetComponent<Button>();
+        toggler.onClick.AddListener(delegate { ToggleMusic(); });
+        imgmusic = GameObject.Find("MusicImage").GetComponent<Image>();
+        musicon = Resources.Load<Sprite>("musicon");
+        musicoff = Resources.Load<Sprite>("musicoff");
     }
 
     public void Start()
     {
         GetSlider();
+        GetToggler();
         SceneManager.sceneLoaded += OnNewSceneLoaded;
         audioData = GetComponent<AudioSource>();
         audioData.volume = slider.value;
@@ -27,19 +42,20 @@ public class MusicScript : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
     }
 
-    public void SoundSlide()
+    void SoundSlide()
     {
         audioData.volume = slider.value;
     }
 
     void OnNewSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        Debug.Log("OnSceneLoaded: " + scene.name);
-        Debug.Log(mode);
+        Debug.Log("New Scene Loaded and MusicScript detected it.");
     }
 
-    //public void ToogleMusic()
-    //{
-    //    audioData.mute = !audioData.mute;
-    //}
+    public void ToggleMusic()
+    {
+        Debug.Log("Music Toggle");
+        audioData.mute = !audioData.mute;
+        imgmusic.sprite = audioData.mute ? musicoff : musicon; 
+    }
 }
